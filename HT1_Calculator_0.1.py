d89610e0e1401277831f19224f6e3f9e517fc379
Olexandrit <itvisa1c@gmail.com>
2022-04-13 09:06:53 +0300

    HT1_Calculator_0.1.py

---
+++ HT1_Calculator_0.1.py
@@ -0,0 +1,18 @@
+
+def calculator():
+    
+    print("Вас вітає НеЙмОвІрНиЙ Калькулятор 0.1\n Я вмію тільки додавати 2 цілих числа")
+    
+    string1 = input("Введіть перше число: ")
+    
+    string2 = input("Введіть друге число: ")
+    
+    num1 = int(string1)
+    
+    num2 = int(string2)
+    
+    sum = num1 + num2
+    
+    print(num1, "+", num2, "=", sum)
+    
+calculator()
