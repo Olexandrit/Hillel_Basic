441f5213a62081d63a85193dc6451e00ac2fd299
Olexandrit <itvisa1c@gmail.com>
2022-04-13 19:44:54 +0300

    1

---
+++ lesson2/comparison.py
@@ -0,0 +1,6 @@
+
+print(1 == 1)
+
+print(1 != 1)
+
+print(str(3) in '123')
---
+++ lesson2/logic.py
@@ -0,0 +1,14 @@
+hanry_is_duck = True
+quacker_is_duck = True
+
+me_is_duck = False
+
+print(hanry_is_duck and quacker_is_duck)
+
+print(hanry_is_duck and me_is_duck)
+
+print(hanry_is_duck or me_is_duck)
+
+text = 'cucumber'
+
+print('z' not in text)
